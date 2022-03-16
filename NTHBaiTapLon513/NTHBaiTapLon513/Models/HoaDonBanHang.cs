ce09306bfa873f0e4e99bda2395278aa3895e764
hieunguyen <hieunguyen@DESKTOP-O8TN4L4>
2022-03-16 10:32:19 +0700

    15/3

---
+++ NTHBaiTapLon513/NTHBaiTapLon513/Models/HoaDonBanHang.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace NTHBaiTapLon513.Models
+{
+    public class HoaDonBanHang
+    {
+        [Key]
+        public string MaHoaDon { get; set; }
+        public string NgayBan { get; set; }
+        public string MaHang { get; set; }
+        public string Soluong { get; set; }
+        public string DonGia { get; set; }
+        public string ThanhTien { get; set; }
+        public HangBan HangBan { get; set; }
+    }
+}
